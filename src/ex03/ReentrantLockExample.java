408f20e7158f6f8283fa717e14877a61d8e0e72e
AbdElraoufSabri <abdelraouf.sabri@el-eng.menofia.edu.eg>
2016-11-27 14:23:47 +0200

    ReentrantLock example

---
+++ src/ex03/ReentrantLockExample.java
@@ -0,0 +1,51 @@
+package ex03;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockExample {
+	private static Account account = new Account();
+
+	public static void main(String[] args) throws InterruptedException {
+		ExecutorService executor = Executors.newCachedThreadPool();
+
+		for (int i = 0; i < 100; i++) {
+			executor.execute(new AddAPennyTask());
+		}
+
+		executor.shutdown();
+
+		executor.awaitTermination(Integer.MAX_VALUE, TimeUnit.DAYS);
+
+		System.out.println("What is balance ? " + account.getBalance());
+	}
+
+	public static class AddAPennyTask implements Runnable {
+		public void run() {
+			account.deposit(1);
+		}
+	}
+
+	public static class Account {
+		private int balance = 0;
+
+		int getBalance() {
+			return balance;
+		}
+
+		void deposit(int amount) {
+			try {
+				int newBalance = balance + amount;
+
+				// To make the error more visible
+				Thread.sleep(5);
+
+				balance = newBalance;
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
