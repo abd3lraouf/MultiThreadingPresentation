4db61575aaf45f9a8622141b11eeb45b23344205
AbdElraoufSabri <abdelraouf.sabri@el-eng.menofia.edu.eg>
2016-11-27 14:21:21 +0200

    synchronized example solution step 2

--- src/ex02/SynchronizedExample.java
+++ src/ex02/SynchronizedExample.java
@@ -24,6 +24,13 @@ public class SynchronizedExample {
 		t1.start();
 		t2.start();
 
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		System.out.println("Count is : " + count);
 	}
 
